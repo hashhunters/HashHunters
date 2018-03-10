4a14177ffe70fc0c66e3d420282f4049776237c6
bruteforcer <bruteforcer@nikto.net>
2018-03-10 18:36:12 +0200

    recalculate fee and bounty when someone calls increaseBounty(), add isOwner modifier

--- contracts/HashPuzzle.sol
+++ contracts/HashPuzzle.sol
@@ -4,50 +4,84 @@ pragma solidity ^0.4.18;
 import "./AbstractAlg.sol";
 
 contract HashPuzzle {
-    event HashCracked(address crackedBy, uint256 bounty, bytes message, bytes hash, bytes32 keccak256OfCalcHash);
-    event AttemptFailed(address source, bytes message, bytes hash, bytes32 keccak256OfCalcHash);
-    
+    event HashCracked(
+        address crackedBy, 
+        uint256 bounty, 
+        bytes message, 
+        bytes32 keccak256OfHash, 
+        bytes32 keccak256OfCalcHash
+    );
+
+    event AttemptFailed(
+        address source, 
+        bytes message, 
+        bytes32 keccak256OfHash, 
+        bytes32 keccak256OfCalcHash
+    );
+
     address private contractOwner;
-    
-    bytes private hash;
+
+    bytes32 private keccak256OfHash; // keccak256 of hash we search for
     address private hashAlgAddress;
-    uint8 private fee; // (0 to 10 percents max) for contract owner if hash being cracked
-    
-    function HashPuzzle(bytes _hash, address _hashAlgAddress, uint8 _fee) public payable {
-        require(0 <= _fee && _fee <= 10);
+    uint8 private percentsFee;
+    uint256 private fee;
+    uint256 private bounty;
+
+    function HashPuzzle(
+        bytes32 _keccak256OfHash, 
+        address _hashAlgAddress, 
+        uint8 _percentsFee
+    ) public payable {
+        // 0 to 10 percents fee maximum for contract owner if hash being cracked
+        require(0 <= _percentsFee && _percentsFee <= 10);
         
         // In a simple call chain A -> B -> C -> D, 
         // inside D msg.sender will be C, and tx.origin will be A.
         contractOwner = msg.sender;
         
-        hash = _hash;
+        keccak256OfHash = _keccak256OfHash;
         hashAlgAddress = _hashAlgAddress;
-        fee = _fee;
+        percentsFee = _percentsFee;
+        fee = (msg.value / 100) * percentsFee;
+        bounty = msg.value - fee;
     }
     
-    function increaseBounty() public payable {
+    modifier isOwner() {
+        require(msg.sender == contractOwner);
+        _;
     }
-    
+
+    function increaseBounty() public isOwner payable {
+        fee = (this.balance / 100) * percentsFee;
+        bounty = this.balance - fee;
+    }
+
     function checkBounty() public view returns (uint256) {
-       return this.balance;
+        return bounty;
     }
-    
+
     function solve(bytes _message) public returns (bool) {
         AbstractAlg abstractAlg = AbstractAlg(hashAlgAddress);
         bytes32 keccak256OfCalcHash = abstractAlg.keccak256OfCalcHash(_message);
 
-        if (keccak256OfCalcHash == keccak256(hash)) {
-            uint256 bounty = this.balance;
-            uint256 ownerFee = (this.balance / 100) * fee;
-
-            contractOwner.transfer(ownerFee);
-            msg.sender.transfer(this.balance);
+        if (keccak256OfCalcHash == keccak256OfHash) {
+            uint256 feeBeforeTransfer = fee;
+            fee = 0;
             
-            HashCracked(msg.sender, bounty, _message, hash, keccak256OfCalcHash);
+            uint256 bountyBeforeTransfer = bounty;
+            bounty = 0;
+            
+            // send fee
+            contractOwner.transfer(feeBeforeTransfer);
+        
+            // send bounty
+            msg.sender.transfer(bountyBeforeTransfer);
+            
+            HashCracked(msg.sender, bounty, _message, keccak256OfHash, keccak256OfCalcHash);
             return true;
         }
     
-        AttemptFailed(msg.sender, _message, hash, keccak256OfCalcHash);
+        AttemptFailed(msg.sender, _message, keccak256OfHash, keccak256OfCalcHash);
         return false;
     }
 }
